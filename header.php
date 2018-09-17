9e1d24cfb07a2ae9d7186a94434d37885fe735f6
Juha Stenroos <juha.stenroos@edita.fi>
2018-09-17 23:45:40 +0300

    add foundation xy-grid and all wc template files

--- header.php
+++ header.php
@@ -6,8 +6,7 @@
   <?php wp_head(); ?>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Foundation -->
-  <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.5.0-rc.3/dist/js/foundation.min.js" integrity="sha256-l1HhyJ0nfWQPdwsVJLNq8HfZNb3i1R9M82YrqVPzoJ4= sha384-NH8utV74bU+noXiJDlEMZuBe34Bw/X66sw22frHkgIs8R1QKWc8ckxYB4DheLjH4 sha512-JMs3Y+JjY+DhwVOPeJhkLM/0FeK9ANxvuYiHGpGp7Q2kMlmNEN/2v6TkrXdirxqB3DHxPlQ8iMxvb/eSPCF5CA==" crossorigin="anonymous"></script>
+ 
   <!-- Animate.css -->
   <link rel="stylesheet"
   href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
-
+<div class="row">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 <h1><?php the_title(); ?></h1>
-<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 <p><?php the_content(__('(more...)')); ?></p>
 
 <?php endwhile; else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; ?>
-
+</div>
 
 <?php get_footer(); ?>
